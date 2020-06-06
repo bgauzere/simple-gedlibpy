0a6378a659beabeacf3f834c9dec3fc38fda584b
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2020-06-06 14:42:27 +0200

    simple test with data

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple GedLibPy
+
+These small codes aim to help the computation of a ged between two graphs
+
+# How to run test.py
+ - Clone and install https://github.com/bgauzere/gedlibpy
+ - modify env PYTHONPATH to include your gedlibpy directory
+`export PYTHONPATH=$PYTHONPATH:/path/to/gedliby/`
+ - `pipenv install`
+ - `pipenv run python test.py` should work and print approximated ged 
--- test.py
+++ test.py
@@ -7,11 +7,6 @@ def compute_ged(Gi, Gj, edit_cost, method='BIPARTITE'):
     """
     Compute GED between two graph according to edit_cost
     """
-    if(Gi.order() > Gj.order()):
-        tmp = Gi
-        Gi = Gj
-        Gj = tmp
-
     dis, pi_forward, pi_backward = GED(Gi, Gj, lib='gedlibpy',
                                        cost='CONSTANT', method=method,
                                        edit_cost_constant=edit_cost,
@@ -21,8 +16,6 @@ def compute_ged(Gi, Gj, edit_cost, method='BIPARTITE'):
     return dis, n_eo_tmp
 
 
-G, y, _ = load_dataset(
-    '/home/bgauzere/work/Recherche/Datasets/Acyclic/dataset_bps.ds')
-
-dis, _ = compute_ged(G[12], G[13], edit_cost=[3, 1, 1, 3, 1, 1])
+G, y, _ = load_dataset('./data/simple_test.ds')
+dis, _ = compute_ged(G[0], G[1], edit_cost=[3, 1, 1, 3, 1, 1])
 print(dis)
